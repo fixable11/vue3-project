e60445d3de902cacf1ef85627a9247d2b40c5a5d
Mykyta Borovyk <fixable11@gmail.com>
2024-10-16 08:34:12 +0300

    Create post component

---
+++ src/views/CreatePostView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import CreatePost from '../components/post/CreatePost.vue'
+</script>
+
+<template>
+  <div class="about">
+    <CreatePost />
+  </div>
+</template>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
